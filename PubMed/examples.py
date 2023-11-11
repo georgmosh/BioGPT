1f704aed74152826e5d77d105b798a0d974e8737
georg_mosh <geomos@aueb.gr>
2023-11-11 19:53:59 +0200

    Add support for PMC-LlaMA in the examples.

--- PubMed/examples.py
+++ PubMed/examples.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 
 from fairseq.models.transformer_lm import TransformerLanguageModel
-from transformers import pipeline, set_seed, BioGptTokenizer, BioGptForCausalLM
+from transformers import pipeline, set_seed, BioGptTokenizer, BioGptForCausalLM, LlamaTokenizer, AutoModelForCausalLM
 
 print("User Current Version:-", sys.version)
 
@@ -32,33 +32,33 @@ def load_state_dict(model, pubmed_biogpt_ckp):
 
 if __name__ == "__main__":
     set_seeds(0)
-    # m = TransformerLanguageModel.from_pretrained(
-    #     "/media/geomos/AUEB BIOMEDICAL DATA/BioGPT/data/BioGPT-Large",
-    #     "/media/geomos/AUEB BIOMEDICAL DATA/BioGPT/checkpoints/Pre-trained-BioGPT-Large/checkpoint.pt",
-    #     data="/media/geomos/AUEB BIOMEDICAL DATA/BioGPT/data/BioGPT-Large",
-    #     tokenizer='moses',
-    #     bpe='fastbpe',
-    #     bpe_codes="data/biogpt_large_bpecodes",
-    #     min_len=100,
-    #     max_len_b=1024)
-    # m.cuda()
-    # src_tokens = m.encode("COVID-19 is")
-    # generate = m.generate([src_tokens], beam=5)[0]
-    # output = m.decode(generate[0]["tokens"])
-    # print(output)
-    #
-    # src_tokens = m.encode("The Effect of chloroquine on cultured fibroblasts is")
-    # generate = m.generate([src_tokens], beam=5)[0]
-    # output = m.decode(generate[0]["tokens"])
-    # print(output)
-    #
-    # load_state_dict(m, "/media/geomos/AUEB BIOMEDICAL DATA/BioGPT/checkpoints/QA-PubMed-BioGPT-Large/checkpoint_avg.pt")
-    #
-    # m.cuda()
-    # src_tokens = m.encode("The Effect of chloroquine on cultured fibroblasts is")
-    # generate = m.generate([src_tokens], beam=5)[0]
-    # output = m.decode(generate[0]["tokens"])
-    # print(output)
+    m = TransformerLanguageModel.from_pretrained(
+        "/media/geomos/AUEB BIOMEDICAL DATA/BioGPT/data/BioGPT-Large",
+        "/media/geomos/AUEB BIOMEDICAL DATA/BioGPT/checkpoints/Pre-trained-BioGPT-Large/checkpoint.pt",
+        data="/media/geomos/AUEB BIOMEDICAL DATA/BioGPT/data/BioGPT-Large",
+        tokenizer='moses',
+        bpe='fastbpe',
+        bpe_codes="data/biogpt_large_bpecodes",
+        min_len=100,
+        max_len_b=1024)
+    m.cuda()
+    src_tokens = m.encode("COVID-19 is")
+    generate = m.generate([src_tokens], beam=5)[0]
+    output = m.decode(generate[0]["tokens"])
+    print(output)
+
+    src_tokens = m.encode("The Effect of chloroquine on cultured fibroblasts is")
+    generate = m.generate([src_tokens], beam=5)[0]
+    output = m.decode(generate[0]["tokens"])
+    print(output)
+
+    load_state_dict(m, "/media/geomos/AUEB BIOMEDICAL DATA/BioGPT/checkpoints/QA-PubMed-BioGPT-Large/checkpoint_avg.pt")
+
+    m.cuda()
+    src_tokens = m.encode("The Effect of chloroquine on cultured fibroblasts is")
+    generate = m.generate([src_tokens], beam=5)[0]
+    output = m.decode(generate[0]["tokens"])
+    print(output)
 
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     model = BioGptForCausalLM.from_pretrained("microsoft/BioGPT-Large")
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     inputs = tokenizer.encode('The Effect of chloroquine on cultured fibroblasts is', return_tensors="pt").to(device)
     model_generation = model.generate(inputs, max_length=100)
     output = tokenizer.decode(model_generation[0], skip_special_tokens=True)
-    print(output)
+    print('BioGPT: ', output)
 
     model = BioGptForCausalLM.from_pretrained("microsoft/BioGPT-Large-PubMedQA")
     tokenizer = BioGptTokenizer.from_pretrained("microsoft/BioGPT-Large-PubMedQA")
@@ -87,4 +87,12 @@ if __name__ == "__main__":
     inputs = tokenizer.encode('The Effect of chloroquine on cultured fibroblasts is', return_tensors="pt").to(device)
     model_generation = model.generate(inputs, max_length=100)
     output = tokenizer.decode(model_generation[0])
-    print(output)
+    print('BioGPT PubMedQA: ', output)
+
+    tokenizer = LlamaTokenizer.from_pretrained('chaoyi-wu/PMC_LLAMA_7B_10_epoch')
+    model = AutoModelForCausalLM.from_pretrained("chaoyi-wu/PMC_LLAMA_7B_10_epoch").half().to(device)
+    inputs = tokenizer('What is the Effect of chloroquine on cultured fibroblasts?', return_tensors="pt", add_special_tokens=False).to(device)
+    with torch.no_grad():
+        model_generation = model.generate(inputs=inputs["input_ids"], max_length=200)  #, do_sample=True, top_k=50
+        output = tokenizer.decode(model_generation[0])
+        print('LlaMA: ', output)
